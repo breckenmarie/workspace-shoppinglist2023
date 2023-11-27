b56b0d496762233adbdcebd98dd1689471c5e196
Brecken Merrill <bmerrill@usca.edu>
2023-11-27 12:51:44 -0500

    11/27 push

--- js/add-element.js
+++ js/add-element.js
@@ -3,10 +3,20 @@
  */
 
 // get all the hot class elements- change them to cool class
+$(".hot").each(function(){
+  //this.setAttibute("class", "cool");
+  $(this).removeClass("hot");
+  //add the class
+  $(this).addClass("cool");
+})
+
+
 
 // traverse the elements
+$("#two").next().next().text("Candy");
 
 // add a new element by clicking the plus sign
+$("#add").click(addElement);
 
 // before and after are for siblings
 // append and prepend are for parent
@@ -14,20 +24,43 @@
 function addElement() {
   // add a new element
   // add a input text box
+  $("#todo").append("<li><input type=\"text\"></li>");
   
   // whenever the user are done add the element
-  
+  $('input').blur(function(){
+    var userinput = $(this).val();
+    $(this).parent().addClass("cool");
+    $(this).parent().text(userinput);
+  });
+  $('li').click(changeStyle);
 }
+
+
 
 // bind click with the event handler
 
+$('li').click(changeStyle);
+
 //  click the li element will change the changeStyle
 //  three style : complete, cool, hot
-function changeStyle() {}
+
+function changeStyle() {
+  if ($(this).hasClass("cool")){
+    $(this).removeClass("cool");
+    $(this).addClass("hot");
+  }
+  else{
+    if ($(this).hasClass("hot")){
+      $(this).removeClass("hot");
+      $(this).addClass("complete");
+    }
+  }
+}
 
 // delete complete element by clicking the trash can
 document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+  $('li.complete').remove();
 }
